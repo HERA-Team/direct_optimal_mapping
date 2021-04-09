0fd7b76a88a327c43d4205404cb2c39c1a3076f6
Zhilei Xu <astro.zlxu@gmail.com>
2021-04-09 09:33:54 -0400

    Updates from development files.

--- optimal_mapping/data_conditioning.py
+++ optimal_mapping/data_conditioning.py
@@ -1,7 +1,6 @@
 import numpy as np
 from pyuvdata import UVData
 
-
 class DataConditioning:
     '''This class takes in the pyuvdata UVData object and perform
     conditioning before the optimal mapping procedure, including
@@ -12,33 +11,31 @@ class DataConditioning:
 
     def __init__(self, uv, ifreq, ipol):
         '''Setting up initial parameters for the object
-
-        Args
+        
+        Input
         ------
         uv: UVData Object
 
         ifreq: integer [0 - 1023]
             select the frequency band
         ipol: integer [-5 - -8]
-            select the linear polarization (-5:-8 (XX, YY, XY, YX))
+            select the linear polarization (-5:-8 (XX, YY, XY, YX))           
 
         '''
-        uv_1d = uv.select(
-            freq_chans=ifreq, polarizations=ipol,
-            inplace=False, keep_all_metadata=False,
-        )
+        uv_1d = uv.select(freq_chans=ifreq, polarizations=ipol, 
+                          inplace=False, keep_all_metadata=False)
         self.uv_1d = uv_1d
 
     def rm_flag(self, uv):
         '''Remove flagged data visibilities, keeping the original
         data object untouched
-
-        Args
+        
+        Input
         ------
         uv: UVData object
             input UVData object to be flag-removed
             The UVData.data_array must only have one dimension along blt
-
+            
         Output
         ------
         uv_flag_rm: UVData object
@@ -47,45 +44,41 @@ class DataConditioning:
         if all(uv.flag_array):
             print('All data are flagged. Returning None.')
             return None
-        idx_t = np.where(uv.flag_array == False)[0]
-        uv_flag_rm = uv.select(
-            blt_inds=idx_t,
-            inplace=False, keep_all_metadata=False,
-        )
+        idx_t = np.where(uv.flag_array==False)[0]
+        uv_flag_rm = uv.select(blt_inds=idx_t, 
+                               inplace=False, keep_all_metadata=False)
         return uv_flag_rm
-
+        
     def redundant_avg(self, uv, tol=1.0):
         '''Averaging within each redundant group, keeping the original
         data object untouched
-
-        Args
+        
+        Input
         ------
         uv: UVData object
             input UVData object to be flag-removed
             The UVData.data_array must only have one dimension along blt
-
+            
         tol: float
             tolerace of the redundant baseline grouping (in meters)
-
+        
         Output
         ------
         uv_red_avg: UVData object
             redundant-baseline averaged UVData object
-
+        
         '''
         # redundant baseline averaging
-        uv_red_avg = uv.compress_by_redundancy(
-            tol=tol,
-            method='average',
-            inplace=False,
-            keep_all_metadata=False,
-        )
+        uv_red_avg = uv.compress_by_redundancy(tol=tol,
+                                               method='average',
+                                               inplace=False,
+                                               keep_all_metadata=False,)
         return uv_red_avg
-
+        
     def redundant_grouping(self, tol=1.0):
         '''Grouping the baselines within the UVData
 
-        Args
+        Input
         ------
         tol: float
             tolerace of the redundant baseline grouping (in meters)
@@ -97,10 +90,10 @@ class DataConditioning:
 
         uvw_groups: list of 2d np.array (float)
             center value of each redundant baseline group
-
+            
         uv_red_avg: UVData obj
             averaged UVData
-
+            
         Attributes
         ------
         .bl_groups: list of lists (int)
@@ -108,27 +101,23 @@ class DataConditioning:
 
         .uvw_groups: list of 2d np.array (float)
             center value of each redundant baseline group
-
+            
         .uv_red_avg: UVData obj
             averaged UVData
         '''
         # redundant baseline grouping
-        bl_groups, uvw_groups, _ = self.uv.get_redundancies(
-            tol=self.tol,
-            use_antpos=False,
-            include_conjugates=False,
-            include_autos=False,
-        )
+        bl_groups, uvw_groups, _ = self.uv.get_redundancies(tol=self.tol, 
+                                                            use_antpos=False, 
+                                                            include_conjugates=False, 
+                                                            include_autos=False)
         # redundant baseline averaging
-        uv_red_avg = self.uv.compress_by_redundancy(
-            tol=self.tol,
-            method='average',
-            inplace=False,
-            keep_all_metadata=False,
-        )
+        uv_red_avg = self.uv.compress_by_redundancy(tol=self.tol,
+                                                    method='average',
+                                                    inplace=False,
+                                                    keep_all_metadata=False,)
         # Attribute assignment
         self.bl_groups = bl_groups
         self.uvw_groups = uvw_groups
         self.uv_red_avg = uv_red_avg
-
+        
         return bl_groups, uvw_groups, uv_red_avg
--- optimal_mapping/optimal_mapping.py
+++ optimal_mapping/optimal_mapping.py
@@ -1,29 +1,28 @@
-import copy
-
-import healpy as hp
 import numpy as np
-from astropy import constants
-from astropy import units as u
-from astropy.coordinates import AltAz
-from astropy.coordinates import EarthLocation
-from astropy.coordinates import FK5
-from astropy.coordinates import SkyCoord
+import numexpr as ne
+import healpy as hp
 from astropy.table import Table
 from astropy.time import Time
+from astropy import constants
+from astropy.coordinates import EarthLocation
+from astropy import units as u
+from astropy.coordinates import AltAz, SkyCoord, FK5
+import copy
+import healpy as hp
 from pyuvdata import UVData
-from scipy.interpolate import RectBivariateSpline as RBS
-from scipy.interpolate import RectSphereBivariateSpline as RSBS
-from scipy.interpolate import SmoothSphereBivariateSpline as SSBS
 
+from scipy.interpolate import SmoothSphereBivariateSpline as SSBS
+from scipy.interpolate import RectSphereBivariateSpline as RSBS
+from scipy.interpolate import RectBivariateSpline as RBS
 
 class OptMapping:
     '''Optimal Mapping Object
     '''
-
+    
     def __init__(self, uv, nside, epoch='J2000'):
         '''Init function for basic setup
-
-        Args:
+         
+        Input
         ------
         uv: pyuvdata object
             UVData data in the pyuvdata format, data_array only has the blt dimension
@@ -33,47 +32,43 @@ class OptMapping:
             epoch of the map, can be either 'J2000' or 'Current'
         Return
         ------
-        None
-
+        None        
+        
         '''
-        self.hera_site = EarthLocation(
-            lat=uv.telescope_location_lat_lon_alt_degrees[0]*u.deg,
-            lon=uv.telescope_location_lat_lon_alt_degrees[1]*u.deg,
-            height=uv.telescope_location_lat_lon_alt_degrees[2]*u.m,
-        )
-
+        self.hera_site = EarthLocation(lat=uv.telescope_location_lat_lon_alt_degrees[0]*u.deg,
+                                       lon=uv.telescope_location_lat_lon_alt_degrees[1]*u.deg,
+                                       height=uv.telescope_location_lat_lon_alt_degrees[2]*u.m)
+        
         self.uv = uv
         self.nside = nside
         self.npix = hp.nside2npix(nside)
         self.hera_dec = self.uv.telescope_location_lat_lon_alt[0]
         self.lsts = np.unique(self.uv.lst_array)
         self.times = np.unique(uv.time_array)
-        if np.mean(self.times) < 2458362:  # 2018-09-01
+        if np.mean(self.times) < 2458362: #2018-09-01
             self.feed_type = 'dipole'
         else:
             self.feed_type = 'vivaldi'
-
+        
         if epoch == 'J2000':
             self.equinox = 'J2000'
         elif epoch == 'Current':
             self.equinox = Time(np.mean(self.times), format='jd')
         else:
             print('Please provide a proper epoch: either J2000 or Current')
-        print('RA/DEC in the epoch of %s.' % self.equinox)
-
+        print('RA/DEC in the epoch of %s.'%self.equinox)
+        
         theta, phi = hp.pix2ang(nside, range(self.npix))
         self.ra = phi
         self.dec = np.pi/2. - theta
-        az, alt = self._radec2azalt(
-            self.ra, self.dec,
-            np.mean(self.times),
-        )
+        az, alt = self._radec2azalt(self.ra, self.dec,
+                                    np.mean(self.times))
         self.az = az
         self.alt = alt
-
+        
         self.frequency = np.squeeze(self.uv.freq_array)
         self.wavelength = constants.c.value/self.frequency
-
+                
         data = np.squeeze(self.uv.data_array)
         flag = np.squeeze(self.uv.flag_array)
         self.data = np.expand_dims(data, axis=1)
@@ -84,8 +79,8 @@ class OptMapping:
     def _radec2azalt(self, ra, dec, time):
         '''Convert ra/dec to az/alt at the given obs_time and assuming the site
         as HERA
-
-        Args
+        
+        Input:
         ------
         ra: 1d array (float)
             array of the ra coordintes (in radians)
@@ -93,7 +88,7 @@ class OptMapping:
             array of the dec coordintes (in radians)
         time: float
             observation time (in the format of JD)
-
+            
         Output:
         ------
         az, alt: 1d array (float)
@@ -104,26 +99,27 @@ class OptMapping:
         c = SkyCoord(ra=ra, dec=dec, unit='radian', frame=FK5, equinox=self.equinox)
         az = np.radians(c.transform_to(aa).az.value)
         alt = np.radians(c.transform_to(aa).alt.value)
-
+        
         return az, alt
-
+        
+    
     def set_k_psf(self, radius_deg, calc_k=False):
         '''Function to set up the K_psf matrix. K_psf selects
-        healpix from the entire sky to the regions within a
+        healpix from the entire sky to the regions within a 
         certain radius away from the phase center
-
-        Args
+        
+        Input:
         ------
         radius_deg: float (in degrees)
             radius to be included in the K_psf matrix
         calc_k: boolean
             whether calculating K_psf
-
+            
         Output:
         ------
         k_psf: 2d array (boolean) (if calc_k=True)
-            Npsf X Npix array
-
+            Npsf X Npix array 
+            
         Attributes:
         ------
         .k_psf_in: 1d array (int)
@@ -148,8 +144,8 @@ class OptMapping:
     def set_beam_model(self, beam_model, interp_method='grid'):
         '''Beam interpolation model set up with RectSphereBivariantSpline
         beam power is used as sqrt(col4**2 + col6**2)
-
-        Args
+        
+        Input:
         ------
         beam_model: str ('vivaldi' or 'dipole')
             beam model used for interpolation
@@ -157,11 +153,11 @@ class OptMapping:
             Method used for interpolating the beam
             'grid' -> RectBivariateSpline
             'sphere' -> RectSphereBivariateSpline
-
+        
         Output:
         ------
         None
-
+        
         Attribute:
         .beam_model: function
             interpolation function for the beam
@@ -174,7 +170,7 @@ class OptMapping:
         else:
             print('Please provide correct beam model (either vivaldi or dipole)')
         ifreq = int(np.round(self.frequency/1e6))
-        beam_file = beam_file_folder+'/farfield (f=%d) [1].txt' % ifreq
+        beam_file = beam_file_folder+'/farfield (f=%d) [1].txt'%ifreq
         beam_table = Table.read(beam_file, format='ascii', data_start=2)
         #print(beam_model, 'is selected with', interp_method, 'interpolation method.')
         beam_theta = np.radians(np.unique(beam_table['col1']))
@@ -193,151 +189,132 @@ class OptMapping:
             print('Please provide a proper interpolation method, either sphere or grid.')
         # Attribute assignment
         self.beam_model = beam_model
-
+        
         return
 
     def set_a_mat(self):
         '''Calculating A matrix, covering the range defined by K_psf
-
-        Args
+        
+        Input:
         ------
         None
-
+        
         Output:
         ------
         a_mat: 2d matrix (complex64)
             a_matrix (Nvis X Npsf) from the given observation
-
+        
         Attribute:
         ------
         .a_mat: 2d matrix (complex64)
             a_matrix added in the attribute
         '''
-
+        
         a_mat = np.zeros((len(self.data), len(self.idx_psf_in)), dtype='float32')
         beam_mat = np.zeros(a_mat.shape, dtype='float32')
         self.set_beam_model(beam_model=self.feed_type)
         for time_t in np.unique(self.uv.time_array):
-            az_t, alt_t = self._radec2azalt(
-                self.ra[self.idx_psf_in],
-                self.dec[self.idx_psf_in],
-                time_t,
-            )
-            lmn_t = np.array([
-                np.cos(alt_t)*np.sin(az_t),
-                np.cos(alt_t)*np.cos(az_t),
-                np.sin(alt_t),
-            ])
+            az_t, alt_t = self._radec2azalt(self.ra[self.idx_psf_in],
+                                            self.dec[self.idx_psf_in],
+                                            time_t)
+            lmn_t = np.array([np.cos(alt_t)*np.sin(az_t), 
+                              np.cos(alt_t)*np.cos(az_t), 
+                              np.sin(alt_t)])
             beam_map_t = self.beam_model(np.pi/2. - alt_t, az_t, grid=False)
             idx_time = np.where(self.uv.time_array == time_t)[0]
             for i in range(len(idx_time)):
                 irow = idx_time[i]
-                a_mat[irow] = +2*np.pi/self.wavelength*np.matmul(
-                    np.matrix(self.uv.uvw_array[irow].astype(np.float32)),
-                    np.matrix(lmn_t.astype(np.float32)),
-                )
+                a_mat[irow] = +2*np.pi/self.wavelength*np.matmul(np.matrix(self.uv.uvw_array[irow].astype(np.float32)),
+                                                                 np.matrix(lmn_t.astype(np.float32)))
                 if self.flag[irow] == False:
                     beam_mat[irow] = beam_map_t.astype(np.float32)
                 elif self.flag[irow] == True:
                     beam_mat[irow] = np.zeros(beam_mat.shape[1])
-                    print('%dth visibility is flagged.' % irow)
+                    print('%dth visibility is flagged.'%irow)
                 else:
-                    print('Flag on the %dth visibility is not recognized.' % irow)
-        a_mat = np.exp(a_mat * 1j)
+                    print('Flag on the %dth visibility is not recognized.'%irow)
+        a_mat = ne.evaluate('exp(a_mat * 1j)')
         a_mat = a_mat.astype('complex64')
         a_mat = np.matrix(a_mat)
         a_mat = np.matrix(np.multiply(a_mat, beam_mat))
         self.a_mat = a_mat
         return a_mat
-
+    
     def set_a_mat_ps(self, ps_radec):
         '''Calculating A matrix, covering the range defined by K_psf
         + the point sources given in the ps_radec arguement
-
-        Args
+        
+        Input:
         ------
         ps_radec: 2d array
-            with shape as n_source X 2, it saves the ra,dec of all
+            with shape as n_source X 2, it saves the ra,dec of all 
             bthe point sources (in radians)
-
+        
         Output:
         ------
         a_mat_ps: 2d matrix (complex64)
             a_matrix (Nvis X (Npsf+Nps)) from the given observation
-
+        
         Attribute:
         ------
         .a_mat_ps: 2d matrix (complex64)
             a_matrix_ps added in the attribute
         '''
-
-        a_mat = np.zeros(
-            (
-                len(self.data), len(self.idx_psf_in) +
-                ps_radec.shape[0],
-            ), dtype='float32',
-        )
+        
+        a_mat = np.zeros((len(self.data), len(self.idx_psf_in)+ps_radec.shape[0]), dtype='float32')
         beam_mat = np.zeros(a_mat.shape, dtype='float32')
         self.set_beam_model(beam_model=self.feed_type)
         ra_ps = ps_radec[:, 0]
         dec_ps = ps_radec[:, 1]
         for time_t in np.unique(self.uv.time_array):
-            az_t, alt_t = self._radec2azalt(
-                np.concatenate((self.ra[self.idx_psf_in], ra_ps)),
-                np.concatenate(
-                    (self.dec[self.idx_psf_in], dec_ps),
-                ),
-                time_t,
-            )
-            lmn_t = np.array([
-                np.cos(alt_t)*np.sin(az_t),
-                np.cos(alt_t)*np.cos(az_t),
-                np.sin(alt_t),
-            ])
+            az_t, alt_t = self._radec2azalt(np.concatenate((self.ra[self.idx_psf_in], ra_ps)),
+                                            np.concatenate((self.dec[self.idx_psf_in], dec_ps)),
+                                            time_t)
+            lmn_t = np.array([np.cos(alt_t)*np.sin(az_t), 
+                              np.cos(alt_t)*np.cos(az_t), 
+                              np.sin(alt_t)])
             beam_map_t = self.beam_model(np.pi/2. - alt_t, az_t, grid=False)
             idx_time = np.where(self.uv.time_array == time_t)[0]
             for i in range(len(idx_time)):
                 irow = idx_time[i]
-                a_mat[irow] = +2*np.pi/self.wavelength*np.matmul(
-                    np.matrix(self.uv.uvw_array[irow].astype(np.float32)),
-                    np.matrix(lmn_t.astype(np.float32)),
-                )
+                a_mat[irow] = +2*np.pi/self.wavelength*np.matmul(np.matrix(self.uv.uvw_array[irow].astype(np.float32)),
+                                                                 np.matrix(lmn_t.astype(np.float32)))
                 if self.flag[irow] == False:
                     beam_mat[irow] = beam_map_t.astype(np.float32)
                 elif self.flag[irow] == True:
                     beam_mat[irow] = np.zeros(beam_mat.shape[1])
-                    print('%dth visibility is flagged.' % irow)
+                    print('%dth visibility is flagged.'%irow)
                 else:
-                    print('Flag on the %dth visibility is not recognized.' % irow)
-        a_mat = np.exp(a_mat * 1j)
+                    print('Flag on the %dth visibility is not recognized.'%irow)
+        a_mat = ne.evaluate('exp(a_mat * 1j)')
         a_mat = a_mat.astype('complex64')
         a_mat = np.matrix(a_mat)
         a_mat = np.matrix(np.multiply(a_mat, beam_mat))
         self.a_mat_ps = a_mat
         return a_mat
-
+    
     def set_noise_mat(self):
         '''Calculating the noise matrix
         '''
         noise_mat = np.diag(np.ones(self.a_mat.shape[0]))
 
         return noise_mat
-
+    
     def set_p_mat(self, facet_radius_deg=7):
         '''Calculating P matrix, covering the range defined by K_psf,
         projectin to the range defined by K_facet
-
-        Args
+        
+        Input:
         ------
         None
-
+        
         Output:
         ------
         p_mat: 2d matrix (complex64) n_k_facet X n_k_psf
             p_matrix from the given observation
         p_diag: 1d array (complex64)
             normalization array for the map within the facet
-
+        
         Attribute:
         ------
         .p_mat: 2d matrix (complex64)
@@ -345,7 +322,7 @@ class OptMapping:
         .p_diag: 1d array (complex64)
             normalization array for the map within the facet
         '''
-        # p_matrix set up
+        #p_matrix set up
         noise_mat = self.set_noise_mat()
         k_facet = np.matrix(self.set_k_facet(radius_deg=facet_radius_deg, calc_k=True))
         p_mat1 = np.matmul(k_facet, self.a_mat.H)
@@ -353,31 +330,31 @@ class OptMapping:
         p_mat2 = self.a_mat
         p_mat = np.matmul(p_mat1, p_mat2)
         p_mat = np.matrix(np.real(p_mat))
-        # normalizatoin factor set up
+        #normalizatoin factor set up
         k_facet_transpose = np.matrix(k_facet.T)
-        p_mat_facet = np.matmul(p_mat, k_facet_transpose)
+        p_mat_facet = np.matmul(p_mat, k_facet_transpose) 
         p_diag = np.diag(p_mat_facet)
         del noise_mat, k_facet, p_mat1, p_mat2, p_mat_facet
-
-        # attribute assignment
+        
+        #attribute assignment
         self.p_mat = p_mat
         self.p_diag = p_diag
         return p_mat, p_diag
-
+    
     def set_p_mat_ps(self, facet_radius_deg=7):
-        '''Calculating P matrix with stand-alone point sources,
+        '''Calculating P matrix with stand-alone point sources, 
         covering the range defined by K_psf + point source pixels,
         projectin to the range defined by K_facet
-
-        Args
+        
+        Input:
         ------
         None
-
+        
         Output:
         ------
         p_mat_ps: 2d matrix (complex64) n_k_facet X (n_k_psf + n_ps)
             p_matrix_ps from the given observation
-
+        
         Attribute:
         ------
         .p_mat_ps: 2d matrix (complex64)
@@ -386,7 +363,7 @@ class OptMapping:
         if not hasattr(self, 'a_mat_ps'):
             print('A matrix with point sources pixel is not set up, returning None.')
             return
-        # p_matrix_ps set up
+        #p_matrix_ps set up
         #noise_mat = self.set_noise_mat()
         k_facet = np.matrix(self.set_k_facet(radius_deg=facet_radius_deg, calc_k=True))
         p_mat1 = np.matmul(k_facet, self.a_mat.H)
@@ -394,30 +371,31 @@ class OptMapping:
         p_mat2 = self.a_mat_ps
         p_mat_ps = np.matmul(p_mat1, p_mat2)
         p_mat_ps = np.matrix(np.real(p_mat_ps))
-        # normalizatoin factor set up
+        #normalizatoin factor set up
         #k_facet_transpose = np.matrix(k_facet.T)
-        #p_mat_facet = np.matmul(p_mat, k_facet_transpose)
+        #p_mat_facet = np.matmul(p_mat_ps[:, :len(opt_map.idx_psf_in)], k_facet_transpose) 
         #p_diag = np.diag(p_mat_facet)
-        #del noise_mat, k_facet, p_mat1, p_mat2, p_mat_facet
+        #del noise_mat, k_facet, k_facet_transpose, p_mat1, p_mat2, p_mat_facet
         del k_facet, p_mat1, p_mat2
-
-        # attribute assignment
+        
+        #attribute assignment
         self.p_mat_ps = p_mat_ps
         return p_mat_ps
-
+        
+    
     def set_k_facet(self, radius_deg, calc_k=False):
         '''Calculating the K_facet matrix
-
-        Args
+        
+        Input:
         ------
         radius: float (in degrees)
             radius to be included in the K_facet matrix
-
+            
         Output:
         ------
         k_facet: 2d array (boolean)
-            Nfacet X Npsf array
-
+            Nfacet X Npsf array 
+            
         Attributes:
         ------
         .k_facet_in: 1d array (int)
@@ -429,13 +407,11 @@ class OptMapping:
         '''
         facet_radius = np.radians(radius_deg)
         self.idx_facet_in = np.where((np.pi/2. - self.alt) < facet_radius)[0]
-        self.idx_facet_out = np.where((np.pi/2. - self.alt) > facet_radius)[0]
-
+        self.idx_facet_out = np.where((np.pi/2. - self.alt) > facet_radius)[0]       
+        
         if calc_k:
             k_full = np.diag(np.ones(len(self.idx_psf_in), dtype=bool))
-            idx_facet_out_psf = np.where(
-                (np.pi/2. - self.alt[self.idx_psf_in]) > facet_radius,
-            )[0]
+            idx_facet_out_psf = np.where((np.pi/2. - self.alt[self.idx_psf_in]) > facet_radius)[0]
             k_facet = np.delete(k_full, idx_facet_out_psf, axis=0)
             del k_full
             self.k_facet = k_facet

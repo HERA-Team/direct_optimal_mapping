09cc07b52ac09862b635d275dd2bb951a556729d
Zhilei Xu <astro.zlxu@gmail.com>
2023-03-10 07:03:59 -0700

    Revert "Changing phase handling from the new pyuvdata."
    This reverts commit 0c13cd3d795194d96b98b1f8e4830df866b302be.

--- src/direct_optimal_mapping/data_conditioning.py
+++ src/direct_optimal_mapping/data_conditioning.py
@@ -22,7 +22,8 @@ class DataConditioning:
         ipol: integer [-5 - -8]
             select the linear polarization (-5:-8 (XX, YY, XY, YX))
         '''
-        uv.unproject_phase()
+        if uv.phase_type != 'drift':
+            raise RuntimeError('Phase_type has to be drift for the data.')
         self.ifreq = ifreq
         self.ipol = ipol
         uv_cross = uv.select(ant_str='cross', inplace=False)

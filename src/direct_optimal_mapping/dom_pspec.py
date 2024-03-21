Fixing the solid angle calculation of RA/Dec cells.

src/direct_optimal_mapping/dom_pspec.py
@@ -90,9 +90,11 @@ class PS_Calc:
         self.n_voxel = self.nx*self.ny*self.nz
 
         dist_cm_h_avg = np.mean(self.dist_cm_h)
+        
+        self.dec_center_deg = np.mean(self.px_dic['dec_deg'][0, :])
 
         self.res_x_deg = np.mean(np.diff(self.px_dic['ra_deg'][:, 0]))
-        self.res_x_mpch = np.radians(self.res_x_deg)*dist_cm_h_avg.value
+        self.res_x_mpch = np.radians(self.res_x_deg) * dist_cm_h_avg.value * np.cos(np.radians(self.dec_center_deg)) 
 
         self.res_y_deg = np.mean(np.diff(self.px_dic['dec_deg'][0, :]))
         self.res_y_mpch = np.radians(self.res_y_deg)*dist_cm_h_avg.value

src/direct_optimal_mapping/optimal_mapping.py
@@ -67,8 +67,7 @@ class SkyPx:
         edge_ra, edge_dec = np.mgrid[ra_ctr_deg - ra_rng_deg/2.:ra_ctr_deg + ra_rng_deg/2.:(n_ra+1)*1j,
                                      dec_ctr_deg - dec_rng_deg/2.:dec_ctr_deg + dec_rng_deg/2.:(n_dec+1)*1j]
         delta_phi = np.radians(edge_ra[1:, :-1]) - np.radians(edge_ra[:-1, :-1])
-        edge_dec_shift = edge_dec - dec_ctr_deg #Moving the horizon to the dec center to minize the curve-sky effect
-        delta_sin_theta = np.sin(np.radians(edge_dec_shift[:-1, 1:])) - np.sin(np.radians(edge_dec_shift[:-1, :-1]))
+        delta_sin_theta = np.sin(np.radians(edge_dec[:-1, 1:])) - np.sin(np.radians(edge_dec[:-1, :-1]))
         delta_sa = delta_phi*delta_sin_theta
         # pixel center
         ra_res_deg = ra_rng_deg/n_ra
67c3211e74be698004195da0568b39f763d57fe7
jnhewitt <jhewitt@mit.edu>
2024-11-17 10:13:47 -0500

    New class:  HorizonMap A map with RA/Dec coordinate grid with cell size matched to integration time of the visibility data.  Give it a data conditioning object and a coordinate grid.  A-matrix is only for one time stamp, and the computed maps are rotated to the RA/Dec grid.

---
+++ src/direct_optimal_mapping/horizon_map.py
@@ -0,0 +1,136 @@
+import numpy as np
+from pyuvdata import UVData
+from direct_optimal_mapping import optimal_mapping
+from direct_optimal_mapping import data_conditioning
+import copy
+
+class HorizonMap:
+    '''This class takes the optimal_mapping data conditioning object
+       and computes the map in horizon coordinates, using a single-time-stamp matrix
+       and rotations to speed up the calculation.
+    '''
+
+    def __init__(self, dc, ra_ctr_deg, ra_rng_deg, dec_ctr_deg, dec_rng_deg,
+                 wts='optimal', norm='one-beam', epoch_map='J2000', uvw_sign=1):
+
+        '''
+        Parameters
+        ----------
+        dc: data conditioning object (one dimensional: one frequency, one polarization)
+        ra_ctr_deg:  RA coordinate of the center of the map in degrees
+        dec_ctr_deg:  Dec coordinate of the center of the map in degrees
+        ra_rng_deg:  Width of map in coordinate degrees
+        dec_rng_deg:  Height of map in degrees
+        wts:  weighting given to visibility points
+        norm:  normalization computed and applied to the map
+        epoch_map:  epoch of the map coordinate system
+        uvw_sign:  same convention as optimal mapping object
+      
+        Note: ra and dec ranges are specified in COORDINATE degrees
+        Example: for a 10-degree ARC on the sky at dec=30deg, specify 10deg/0.8666
+        East-West cell size will be one time integration
+        North-South cell size will be set to equal the East-West cell size
+        '''
+
+        self.dc = dc
+        self.ra_ctr_deg = ra_ctr_deg
+        self.dec_ctr_deg = dec_ctr_deg
+        self.ra_rng_deg = ra_rng_deg
+        self.dec_rng_deg = dec_rng_deg
+        self.wts = wts
+        self.norm = norm
+        self.epoch_map = epoch_map
+        self.uvw_sign = uvw_sign 
+
+        if wts != 'optimal':
+            print('Only optimal weighting is implemented so far')
+        if norm != 'one-beam':
+            print('Only one-beam normalization is implemented so far')
+
+        return
+    
+    def calc_map(self):
+
+        #
+        # set up pixels
+        #
+        nra=self.ra_rng_deg*3600./(self.dc.uv_1d.integration_time[0]*15.)
+        nra=int(nra)
+        ndec=self.dec_rng_deg*3600./(self.dc.uv_1d.integration_time[0]*15.*np.cos(self.dec_ctr_deg*np.pi/180.))
+        ndec=int(ndec)
+        print('map coordinate grid is: (center,range,number)')
+        print('ra: ',self.ra_ctr_deg,self.ra_rng_deg,nra,' dec: ',self.dec_ctr_deg,self.dec_rng_deg,ndec)
+        temp=self.ra_rng_deg*np.cos(self.dec_ctr_deg*np.pi/180.)/nra
+        print('cell sizes in arc deg based on int time are ',temp,self.dec_rng_deg/ndec,)
+        pixels = optimal_mapping.SkyPx()
+        pixel_dict=pixels.calc_radec_pix(self.ra_ctr_deg,self.ra_rng_deg,nra,self.dec_ctr_deg,self.dec_rng_deg,ndec)
+        #
+        # compute noise matrix (assumed diagonal)
+        #
+        self.dc.noise_calc()
+        #
+        # check whether noise was calculated (noise array is 1D)
+        #
+        if (np.sum(self.dc.uvn.data_array)==len(self.dc.uvn.data_array)):
+            print('Unable to calculate noise:  sigma-n is ones')
+        #
+        # set reference at center of time span
+        #
+        ulsts=np.unique(self.dc.uv_1d.lst_array)
+        ref_lst_index=int(len(ulsts)/2-1)
+        self.ref_lst_deg=ulsts[ref_lst_index]*180./np.pi
+        print('Reference LST index is ',ref_lst_index)
+        print('Reference LST is ',self.ref_lst_deg,' degrees')
+        uvref=self.dc.uv_1d.select(lst_range=[ulsts[ref_lst_index],ulsts[ref_lst_index]],inplace=False)
+        dcref = data_conditioning.DataConditioning(uvref,0,self.dc.ipol)
+        del uvref
+        print('Reference data array shape ',dcref.uv_1d.data_array.shape)
+        #
+        # create map object and compute A-matrix components and weights
+        #
+        print('Creating map object')
+        opt_map = optimal_mapping.OptMapping(dcref.uv_1d,pixel_dict,px_dic_inner=pixel_dict,epoch=self.epoch_map)
+        print('map object created')
+        opt_map.set_phase_mat(uvw_sign=self.uvw_sign)
+        print('Phase matrix shape is ',opt_map.phase_mat.shape)
+        opt_map.set_beam_mat(uvw_sign=1)
+        print('Beam matrix shape is ',opt_map.beam_mat.shape)
+        # compute weights
+        if self.wts=='optimal':
+            opt_map.set_inv_noise_mat(self.dc.uvn,matrix=False)
+            print('inv noise shape ',opt_map.inv_noise_mat.shape)
+        #
+        # compute maps
+        #
+        self.avmap=np.zeros((nra,ndec))
+        self.b1map=np.zeros((nra,ndec))
+        amatrix=opt_map.phase_mat*opt_map.beam_mat
+        for itime, time_stamp in enumerate(np.unique(self.dc.uv_1d.time_array)):
+            idx_roll = itime - ref_lst_index
+            print('Computing map for timestamp ',itime, time_stamp,' - roll is ',idx_roll)
+            idx_t=np.where(self.dc.uv_1d.time_array==time_stamp)[0]
+            vis=self.dc.uv_1d.data_array[idx_t,0,0]
+            wts=opt_map.inv_noise_mat[idx_t]
+            if (np.sum(self.dc.uv_1d.flag_array[idx_t])==vis.shape[0]):
+                print('All flagged for this time stamp - skip it')
+            else:  
+                # there is good data, so compute time-stamp map and add to accum array
+                avmapt=np.real(np.matmul(np.conjugate(amatrix).T,np.multiply(wts,vis)))
+                b1mapt=np.real(np.matmul(np.conjugate(opt_map.beam_mat).T,np.multiply(wts,vis*0.+1.)))
+                self.avmap=self.avmap+np.roll(avmapt.reshape(nra,ndec),idx_roll,axis=0)
+                self.b1map=self.b1map+np.roll(b1mapt.reshape(nra,ndec),idx_roll,axis=0)
+        self.avmap=self.avmap/self.b1map
+        self.avmap=np.real(self.avmap)
+        self.b1map=np.real(self.b1map)
+        #
+        # pack everything into the dictionary.  Don't yet have P
+        #
+        mapdict={'px_dic':pixel_dict}
+        mapdict['map_sum']=self.avmap
+        mapdict['weight_sum']=self.b1map
+        mapdict['n_vis']=self.dc.uv_1d.data_array.shape[0]  # Zhilei's def.  Does not include nsamples
+        mapdict['freq']=self.dc.uv_1d.freq_array[0] # in Hz
+        mapdict['polarization']=self.dc.ipol
+        mapdict['bl_max']=np.sqrt(np.sum(self.dc.uv_1d.uvw_array**2, axis=1)).max()
+        mapdict['radius2ctr']=0.  # distance of pixels to center.  Do we really need this/
+        return mapdict
